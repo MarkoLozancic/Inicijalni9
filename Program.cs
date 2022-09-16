9aac9ee7579dde2f63180c6fac981fbe3e1c4c8c
User <User@LOZA>
2022-09-16 12:26:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalni9
+{
+    internal class Program
+    {
+        //Napišite program koji će definirati niz tipa int, te će ga predefinirati,
+          //  odnosno sortirati tako da članovi budu poredani od manjeg prema većem.Program mora raditi ispravno za proizvoljni broj članova niza. Članove niza na kraju ispišite.
+        static void Main(string[] args)
+        {
+            int[] p = new int[] { 3, 4, 5, 6, 1, 2, 7, 8, 9 };
+            for (int i = 0; i < p.Length; i++)
+            { Console.WriteLine(p[i]); }
+            Console.WriteLine("Sortirani niz:\n");
+           Array.Sort(p);
+            for(int i=0; i<p.Length; i++)
+            { Console.WriteLine(p[i]);
+               
+            }
+            Console.ReadKey();
+      }
+    }
+}
